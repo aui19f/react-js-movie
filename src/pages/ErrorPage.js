411b597bdd6bc289734b64bf9bbc30bed035eba7
aui19f <aui19f@gmail.com>
2024-09-27 20:10:03 +0900

    404 페이지 생성 후 홈으로 이동버튼 추가

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,3 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./ErrorPage.module.css";
 export default function ErrorPage() {
-  return <p>ERROR PAGE..3</p>;
+  const navigate = useNavigate();
+
+  const pageMove = () => {
+    navigate("/");
+  };
+  return (
+    <div className={styles.errorPage}>
+      <h1>404</h1>
+      <p>Page Not Found</p>
+      <button onClick={pageMove}>Go Home</button>
+    </div>
+  );
 }
